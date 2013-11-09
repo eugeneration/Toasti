f04b70b5635c1a3e9dfa609bf7579e5b6bbe91ce
Eugene Lee <eugene@talkto.com>
2013-11-09 17:10:22 -0500

    first commit

---
+++ Toasti/src/com/example/Toasti/CallActivity.java
@@ -0,0 +1,17 @@
+package com.example.Toasti;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Eugene
+ * Date: 11/9/13
+ * Time: 4:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CallActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
